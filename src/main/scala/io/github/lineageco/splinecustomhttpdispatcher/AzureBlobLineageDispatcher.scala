52fcbecb3f6be88172b69ec515ba7afc5d191c5c
Nikumbh <ganesh.a.nikumbh@capgemini.com>
2023-10-27 21:40:20 +0530

    1.1.3

--- src/main/scala/io/github/lineageco/splinecustomhttpdispatcher/AzureBlobLineageDispatcher.scala
+++ src/main/scala/io/github/lineageco/splinecustomhttpdispatcher/AzureBlobLineageDispatcher.scala
@@ -9,9 +9,7 @@ import scala.util.control.NonFatal
 //import za.co.absa.spline.harvester.exception.SplineInitializationException
 import za.co.absa.spline.producer.model.{ExecutionEvent, ExecutionPlan}
 import za.co.absa.spline.harvester.dispatcher.LineageDispatcher
-import com.databricks.dbutils_v1.DBUtilsHolder.dbutils
-import com.azure.identity.ClientSecretCredentialBuilder
-import com.azure.storage.file.datalake.DataLakeServiceClientBuilder
+
 
 /**
  * AzureBlobLineageDispatcherConfig is responsible for sending the lineage data to Azure Blob Storage through the producer API
@@ -19,16 +17,13 @@ import com.azure.storage.file.datalake.DataLakeServiceClientBuilder
 
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import java.nio.file.{Files,Paths}
 import io.github.lineageco.splinecustomhttpdispatcher.AzureBlobLineageDispatcher._
 import za.co.absa.spline.harvester.json.HarvesterJsonSerDe.impl._
 
 class AzureBlobLineageDispatcher(filePath: String,
                                  fileNameKey: String,
-                                 apiVersion: Version,
-                                 accountName: String,
-                                 clientId: String,
-                                 clientSecret: String,
-                                 tenantId: String)
+                                 apiVersion: Version)
   extends LineageDispatcher
     with Logging {
 
@@ -36,30 +31,13 @@ class AzureBlobLineageDispatcher(filePath: String,
   def this(conf: Configuration) = this(
     filePath = conf.getRequiredString(FilePath),
     fileNameKey = conf.getRequiredString(FileNameKey),
-    apiVersion = Version.asSimple(conf.getRequiredString(ApiVersion)),
-    accountName = conf.getRequiredString(AccountName),
-    clientId = conf.getRequiredString(ClientId),
-    clientSecret = conf.getRequiredString(ClientSecret),
-    tenantId = conf.getRequiredString(TenantId)
+    apiVersion = Version.asSimple(conf.getRequiredString(ApiVersion))
   )
   logInfo(s"Using File Path: ${filePath}")
   logInfo(s"Using file name key: ${fileNameKey}")
   logInfo(s"Using api version: ${apiVersion}")
 
   override def name = "Blob"
-
-  // Build the Service Principal credentials
-  val clientSecretCredential = new ClientSecretCredentialBuilder()
-    .clientId(clientId)
-    .clientSecret(clientSecret)
-    .tenantId(tenantId)
-    .build()
-
-  // Build the ADLS Gen2 service client
-  val serviceClient = new DataLakeServiceClientBuilder()
-    .endpoint(s"https://${accountName}.dfs.core.windows.net")
-    .credential(clientSecretCredential)
-    .buildClient()
 
   private val modelMapper = ModelMapper.forApiVersion(apiVersion)
 
@@ -81,37 +59,50 @@ class AzureBlobLineageDispatcher(filePath: String,
     // Get the current timestamp
     val currentDateTime = LocalDateTime.now()
 
-    // Define the formatter for the desired format
-    val formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss")
+    // Define the file timestamp formatter for the desired format
+    val fileTimestampFormatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss")
 
-    // Format the timestamp as a string
-    val timestampString = currentDateTime.format(formatter)
+    // Format the file timestamp as a string
+    val fileTimeStamp = currentDateTime.format(fileTimestampFormatter)
 
-    val fileNameWithDateTime = s"${fileNamePattern}_${timestampString}.json"
+    val fileNameWithDateTime = s"${fileNamePattern}_${fileTimeStamp}.json"
 
-    logInfo(s"sendJson lookslike : \n${json.asPrettyJson}")
+    // Define the directory date formatter for the desired format
+    val dirDateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
 
-    val completeDbfsFilePath = s"${dbfsFilePath}/${fileNameWithDateTime}"
+    // Format the directory date as a string
+    val dirDate = currentDateTime.format(dirDateFormatter)
 
-//    try {
-//      dbutils.fs.put(completeDbfsFilePath, json, true)
-//
-//    } catch {
-//      case NonFatal(e) =>
-//        throw new RuntimeException(s"Cannot write data to ${completeDbfsFilePath}", e)
-//    }
+    val completeDirPath = s"${dbfsFilePath}/date=${dirDate}"
+
+//    logInfo(s"sendJson lookslike : \n${json.asPrettyJson}")
+
+    // Define the path to the local directory and the file name
+    val directoryPath = Paths.get(completeDirPath)
+    val filePath = directoryPath.resolve(fileNameWithDateTime)
+
+
+    try {
+
+      // Create the directory if it doesn't exist
+      if (!Files.exists(directoryPath)) {
+        Files.createDirectories(directoryPath)
+      }
+
+      // Write the JSON data to the file
+      Files.write(filePath, json.getBytes("UTF-8"))
+
+    } catch {
+      case NonFatal(e) =>
+        throw new RuntimeException(s"Cannot write data to ${filePath}", e)
+    }
   }
 
 }
 
 object AzureBlobLineageDispatcher {
   val FilePath = "databricks.filepath"
-  val FileNameKey = "file.name.key"
+  val FileNameKey = "filename.pattern"
   val ApiVersion = "apiVersion"
-  val AccountName = "account.name"
-  val ClientId = "client.id"
-  val ClientSecret = "client.secret"
-  val TenantId = "tenant.id"
-
 }
 
